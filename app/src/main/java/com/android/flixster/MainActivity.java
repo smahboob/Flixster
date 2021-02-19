a42d18b24133034b2f1fcace32f04047bbbd82a5
Saad Mahboob <smahboob@fandm.edu>
2021-02-19 13:36:30 -0500

    Exception Handling

--- app/src/main/java/com/android/flixster/MainActivity.java
+++ app/src/main/java/com/android/flixster/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 catch (JSONException e) { e.printStackTrace();}
             },
             //error
-            error -> { error.printStackTrace(); }
+                Throwable::printStackTrace
         );
 
         mQueue.add(jsonObjectRequest);
